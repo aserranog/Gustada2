1ca0b7db27b01953c0cd90e0fde07ce147874852
Angel <Angel@10.0.2.15>
2014-05-12 12:55:51 +0200

    Version 1.0

--- src/gustada/Gustada.java
+++ src/gustada/Gustada.java
@@ -17,7 +17,7 @@ public class Gustada {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("La Gustada v1.1");
+        System.out.println("La Gustada v1.2");
     }
     
 }
